5117ff98cbcc8893da1bbe9556707f941ba16acd
houssybadr <bad.houssy@gmail.com>
2026-09-21 20:40:33 +0000

    added controller

--- bank-account-service/src/main/java/org/sid/bank_account_service/controllers/BankAccountController.java
+++ bank-account-service/src/main/java/org/sid/bank_account_service/controllers/BankAccountController.java
@@ -1,4 +1,54 @@
 package org.sid.bank_account_service.controllers;
 
+import org.sid.bank_account_service.entities.BankAccount;
+import org.sid.bank_account_service.repositories.BankAccountRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.file.Path;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/bank-account")
 public class BankAccountController {
+
+    private BankAccountRepository repository;
+
+    public BankAccountController(BankAccountRepository repository){
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<BankAccount> findAll(){
+        return this.repository.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public BankAccount findById(@PathVariable String id){
+        return this.repository.findById(id)
+                .orElseThrow(()->new RuntimeException("Account Not Found"+id));
+    }
+
+    @PostMapping
+    public BankAccount save(@RequestBody BankAccount bankAccount){
+        return this.repository.save(bankAccount);
+    }
+
+    @PutMapping(path="{id}")
+    public BankAccount update(
+            @RequestBody BankAccount bankAccount,
+            @PathVariable String id
+    ){
+        BankAccount fetched=this.repository.findById(id)
+                .orElseThrow(()->new RuntimeException("Account Not Found"+id));
+        if(bankAccount.getBalance() != null) fetched.setBalance(bankAccount.getBalance());
+        if(bankAccount.getType() != null) fetched.setType(bankAccount.getType());
+        if(bankAccount.getCurrency() != null) fetched.setCurrency(bankAccount.getCurrency());
+        if(bankAccount.getCreatedAt() != null) fetched.setCreatedAt(bankAccount.getCreatedAt());
+        return this.repository.save(fetched);
+    }
+
+    @DeleteMapping(path="{id}")
+    public void deleteById(@PathVariable String id){
+        this.deleteById(id);
+    }
 }
